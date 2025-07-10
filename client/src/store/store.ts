defa180f79d17e8f761f9cdd74bc436f3246eae9
shrimpcod <prog2708@gmail.com>
2025-07-10 12:00:02 +0500

    feat(client): Добавить redux store хранилище данных

---
+++ client/src/store/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from '@reduxjs/toolkit'
+import authReducer from "./features/auth/authSlice";
+import chatReducer from "./features/chat/chatSlice"
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        chat: chatReducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
