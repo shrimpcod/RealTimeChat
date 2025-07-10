6e33916239267d43554e5b902248d91bd5a419ec
shrimpcod <prog2708@gmail.com>
2025-07-10 11:26:48 +0500

    feat(client): Добавить хук проверки мобильного разрешения экрана

---
+++ client/src/hooks/useIsMobile.tsx
@@ -0,0 +1,13 @@
+import {useState, useEffect} from "react";
+
+const useIsMobile = (breakpoint: number = 767): boolean => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= breakpoint);
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= breakpoint);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [breakpoint])
+    return isMobile;
+}
+
+export default useIsMobile;
